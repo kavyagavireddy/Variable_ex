d245586bf0248d44edbba97afec7d8ef952fce24
kavyagavireddy <165667466+kavyagavireddy@users.noreply.github.com>
2024-04-16 10:12:18 +0530

    Add files via upload

---
+++ Variable_Ex.java
@@ -0,0 +1,27 @@
+package com.odigos.core;
+class Variable_Ex
+{
+public static void main(string args[])
+{
+System.out.println(" Welome to java");
+int salary=12345;
+float result=23.54391279126112f;
+double value=23.54391279126112;
+long contact=9515889569l;
+char gender='f';
+boolean test=true;
+System.out.println(" salary");
+System.out.println(" result");
+System.out.println(" value");
+System.out.println(" contact");
+System.out.println(" gender");
+System.out.println(" test");
+}
+}
+
+
+
+
+
+
+-
